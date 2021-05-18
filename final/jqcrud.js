248c7a53a5bc746264f66fd7bc1b64b71190a971
waqarsheikh07 <waqarsheikh895@gmail.com>
2021-05-18 10:54:28 +0500

    version 1.0

---
+++ final/jqcrud.js
@@ -0,0 +1,100 @@
+$(() => {
+  loadRecipies();
+  $("#recipes").on("click", ".btn-danger", handleDelete);
+  $("#recipes").on("click", ".btn-warning", handleUpdate);
+  $("#addBtn").click(addRecipe);
+  $("#updateSave").click(function () {
+    var id = $("#updateId").val();
+    var title = $("#updateTitle").val();
+    var body = $("#updateBody").val();
+    $.ajax({
+      url: "https://usman-recipes.herokuapp.com/api/recipes/" + id,
+      data: { title, body },
+      method: "PUT",
+      success: function (response) {
+        console.log(response);
+        loadRecipies();
+        $("#updateModal").modal("hide");
+      },
+    });
+  });
+});
+function handleUpdate() {
+  var btn = $(this);
+  var parentDiv = btn.closest(".recipe");
+  let id = parentDiv.attr("data-id");
+  $.get(
+    "https://jsonplaceholder.typicode.com/users/" + id,
+    function (response) {
+      $("#updateId").val(response.id);
+      $("#updatename").val(response.name);
+      $("#updateemail").val(response.email);
+      $("#updateModal").modal("show");
+    }
+  );
+}
+function addRecipe() {
+  var name = $("#name").val();
+  var email = $("#email").val();
+  if (!validateEmail(email)) {
+    alert("invalid email");
+    return;
+  }
+
+  $.ajax({
+    url: "https://jsonplaceholder.typicode.com/users",
+    method: "POST",
+    data: { name, email },
+    success: function (response) {
+      console.log(response);
+      $("#name").val("");
+      $("#email").val("");
+      loadRecipies();
+      $("#addModal").modal("hide");
+    },
+  });
+}
+function handleDelete() {
+  var btn = $(this);
+  var parentDiv = btn.closest(".recipe");
+  let id = parentDiv.attr("data-id");
+  console.log(id);
+  $.ajax({
+    url: "https://jsonplaceholder.typicode.com/users/" + id,
+    method: "DELETE",
+    success: function () {
+      loadRecipies();
+    },
+  });
+}
+function loadRecipies() {
+  $.ajax({
+    url: "https://jsonplaceholder.typicode.com/users",
+    method: "GET",
+    error: function (response) {
+      var recipes = $("#recipes");
+      recipes.html("An Error has occured");
+    },
+    success: function (response) {
+      console.log(response);
+      var recipes = $("#recipes");
+      recipes.empty();
+      for (var i = 0; i < response.length; i++) {
+        var rec = response[i];
+        recipes.append(
+          `<div class="recipe" data-id="${rec.id}"><h3>${rec.name}</h3><p><button class="btn btn-danger btn-sm float-right">delete</button><button class="btn btn-warning btn-sm float-right">Edit</button>${rec.email}</p></div>`
+        );
+      }
+    },
+  });
+}
+function validateEmail(emailField) {
+  var reg = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([com]{3})$/;
+
+  if (reg.test(emailField.value) == false) {
+    alert("Invalid Email Address");
+    return false;
+  }
+
+  return true;
+}
